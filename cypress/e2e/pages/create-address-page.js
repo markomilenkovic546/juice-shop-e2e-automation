7191fa4827e8036fe75484cc25d700e1b6fd99ec
markomilenkovic546 <markomilenkovic546@gmail.com>
2023-07-05 20:51:41 +0200

    address related  page object methods are created

---
+++ cypress/e2e/pages/create-address-page.js
@@ -0,0 +1,52 @@
+import Header from "../components/header";
+
+export default class SelectAddress {
+  constructor() {
+    this.header = new Header();
+  }
+
+  // Get "Country" input field
+  getCountryInputField() {
+    return cy.get('[data-placeholder="Please provide a country."]');
+  }
+
+  // Get "Name" input field
+  getNameInputField() {
+    return cy.get('[data-placeholder="Please provide a name."]');
+  }
+
+  // Get "Number" input field
+  getMobileNumberInputField() {
+    return cy.get('[data-placeholder="Please provide a mobile number."]');
+  }
+
+  // Get "ZipCode" input field
+  getZipCodeInputField() {
+    return cy.get('[data-placeholder="Please provide a ZIP code."]');
+  }
+
+  // Get "Address" text box field
+  getAddressTextBoxInputField() {
+    return cy.get('[data-placeholder="Please provide an address."]');
+  }
+
+  // Get "City" input field
+  getCityInputField() {
+    return cy.get('[data-placeholder="Please provide a city."]');
+  }
+
+  // Get "State" input field
+  getStateInputField() {
+    return cy.get('[data-placeholder="Please provide a state."]');
+  }
+
+  // Get "Back" button
+  getBackButton() {
+    return cy.get("button.btn-return");
+  }
+
+  // Get "Submit" button
+  getSubmitButton() {
+    return cy.get("#submitButton");
+  }
+}
---
+++ cypress/e2e/pages/select-address-page.js
@@ -0,0 +1,24 @@
+import Header from "../components/header";
+
+export default class SelectAddress {
+  constructor() {
+    this.header = new Header();
+  }
+
+// Get "New addres" button
+  getNewAddressButton() {
+    return cypress.get('[aria-label="Add a new address"]')
+  }
+
+  // Get "Continue" button
+  getContinueButton() {
+    return cy.get('[aria-label="Proceed to payment selection"]')
+  }
+
+// Get "Select address" radio button 
+
+getSelectAdressRadio() {
+return cy.get('[type="radio"]')
+}
+
+}
